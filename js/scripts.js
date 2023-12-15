38f33ae6a683d658f98f7c2395466cc793dbfc9a
JohnatanMora <johnatan.mora@alu.uclm.es>
2023-12-15 23:37:06 +0100

    Pequeños cambios

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    document.getElementById("mySidenav").style.width = (document.getElementById("mySidenav").style.width === "250px") ? "0" : "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
